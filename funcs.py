aa30cf048cd46d561bb7b8d011f00a9d09937d11
Dim-ON1975 <zaberov.dv@internet.ru>
2023-09-23 23:24:57 +0300

    Вычисление разности двух целых чисел

--- funcs.py
+++ funcs.py
@@ -6,3 +6,13 @@ def summ_two(num1: int, num2: int) -> int:
     :return: сумма двух чисел, int
     """
     return num1 + num2
+
+
+def diff_two(num1: int, num2: int) -> int:
+    """
+    Функция вычитания двух целых чисел
+    :param num1: целое число, int
+    :param num2: целое число, int
+    :return: разность двух чисел, int
+    """
+    return num1 - num2
