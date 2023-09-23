0931974c2395aa18d98a80e9b3f5335ad9e76f0b
Dim-ON1975 <zaberov.dv@internet.ru>
2023-09-23 23:16:50 +0300

    Вычисление суммы двух целых чисел

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def summ_two(num1: int, num2: int) -> int:
+    """
+    Функция сложения двух чисел
+    :param num1: целое число, int
+    :param num2: целое число, int
+    :return: сумма двух чисел, int
+    """
+    return num1 + num2
