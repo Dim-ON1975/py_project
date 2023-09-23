126f619954086dd71de95691fb34ff869cf6999e
Dim-ON1975 <zaberov.dv@internet.ru>
2023-09-23 23:42:37 +0300

    Результат: сумма и разность двух чисел

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from funcs import summ_two
+import funcs
+from funcs import summ_two, diff_two
 
 num1 = 20
 num2 = 30
+num3 = 40
 
-print(f'{num1} + {num2} = {funcs.summ_two(num1, num2)}')
+print(f'{num1} + {num2} = {summ_two(num1, num2)}')
+print(f'{num3} - {num2} = {diff_two(num3, num2)}')
